2bd41be1f2c60efe8f9f6508da430c6dc3436c80
vishwanath <vishwanathholge18@gmail.com>
2022-04-21 11:06:08 +0530

    added js

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 function calculate(){
-    let balance1 = Number(document.getElementById("bank_balance1").value);
-    let balance2 = Number(document.getElementById("bank_balance2").value);
-    let balance3 = Number(document.getElementById("bank_balance3").value);
-    let balance4 = Number(document.getElementById("bank_balance4").value);
-    let balance5 = Number(document.getElementById("bank_balance5").value);
-    let balance6 = Number(document.getElementById("bank_balance6").value);
-    const balances = [balance1,balance2,balance3,balance4,balance5,balance6];
-    console.log(balances);
-    for(let i = 0; i < balances.length; i++){
-        if(balances[i] != null ){
-            if(balances[i]> 200000){
-                balances[i] = balances[i] * 1.1;
-                balances[i] = balances[i] - balances[i] * 0.002;
+    var inputBalances = document.getElementsByClassName("bank-balance");
+    
+    
+    for(let i =0 ; i < inputBalances.length; i++){
+         var arr;
+         arr[i] = inputBalances[i].value
+    }
+    // let balance1 = Number(document.getElementById("bank_balance1").value);
+    // let balance2 = Number(document.getElementById("bank_balance2").value);
+    // let balance3 = Number(document.getElementById("bank_balance3").value);
+    // let balance4 = Number(document.getElementById("bank_balance4").value);
+    // let balance5 = Number(document.getElementById("bank_balance5").value);
+    // let balance6 = Number(document.getElementById("bank_balance6").value);
+    // const balances = [balance1,balance2,balance3,balance4,balance5,balance6];
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != null ){
+            if(arr[i]> 200000){
+                arr[i] = arr[i]* 1.1;
+                arr[i] = arr[i] - arr[i] * 0.002;
 
             }
-            else if(balances[i] > 100000){
-                balances[i] = balances[i] + balances[i] * 0.05;
-                balances[i] = balances[i] - balances[i] * 0.0005;
+            else if(arr[i] > 100000){
+                arr[i] = arr[i] + arr[i] * 0.05;
+                arr[i] = arr[i] - arr[i] * 0.0005;
             }
             else{
-                balances[i] = balances[i] + balances[i] * 0.03;
+                arr[i] = arr[i] + arr[i] * 0.03;
             }
         }
 
     }
-    console.log(balances);
-    for(let i = 0; i < balances.length; i++){
-        document.getElementById("updated_balance" + Number(i +1)).innerText= balances[i];
+    for(let i =0 ; i < arr.length; i++){
+        console.log(arr[i]);
     }
+    for(let i =0 ; i < arr.length; i++){
+        console.log(inputBalances[i].value);
+    }
+    // console.log(balances);
+    // for(let i = 0; i < balances.length; i++){
+    //     document.getElementById("updated_balance" + Number(i +1)).innerText= balances[i];
+    // }
 
 }
